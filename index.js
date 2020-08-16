4dc5136d781eef1f7b003a4229216772a7c235e8
CodeSalvageON <alexy0730@gmail.com>
2020-08-16 21:27:10 +0000

    Added unique player system

--- index.js
+++ index.js
@@ -1,22 +1,94 @@
+// Install File System and Express
+
 const fs = require('fs');
 const express = require('express');
+
+// Create the server
 
 var app = require('express')();
 var http = require('http').Server(app);
 var bodyParser = require('body-parser');
 
+// Set Port and Socket
+
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
+
+// Set Target Folder
 
 app.use(bodyParser.json());
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// Create an Array
+var playerArray = [];
+var playerAttributeArray = [];
+
+// Static Pages
 
 app.get('/', function(req, res){
   var static_path = __dirname+'/public/static/index.html';
 
   res.sendFile(static_path);
 });
+
+// Post Requests
+
+app.post('/join_game', function(req, res){
+  // Get User Data from POST request
+
+  const user_name = req.body.username;
+  const user_creature = req.body.creature;
+  const user_game_type = req.body.game_type;
+
+  // Check if a user with the same name is on 
+
+  if (playerArray.includes(user_name)){
+    // Give the player a unique username
+    
+    var number = 0;
+    var unique_user = user_name;
+
+    function createUniqueUser() {
+      if (playerArray.includes(unique_user)){
+        number = number + 1;
+
+        unique_user = user_name + '_' + number;
+        createUniqueUser();
+      }
+      else{
+        // Create the Unique User
+
+        playerArray.push(unique_user);
+        console.log(playerArray);
+
+        // Add the player's attributes
+
+        var attribute_code = unique_user + '_' + user_creature + '_' + user_game_type;
+
+        playerAttributeArray.push(attribute_code);
+        console.log(playerAttributeArray);
+      }
+    }
+
+    createUniqueUser();
+  }
+  else{
+    // Create the User
+
+    playerArray.push(user_name);
+    console.log(playerArray);
+
+    // Add the player's attributes
+
+    var attribute_code = user_name + '_' + user_creature + '_' + user_game_type;
+
+    playerAttributeArray.push(attribute_code);
+    console.log(playerAttributeArray);
+  }
+});
+
+// Listen on the set port
 
 http.listen(port, function(){
   console.log('listening on *:' + port);
